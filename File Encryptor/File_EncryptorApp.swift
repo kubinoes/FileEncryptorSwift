1e6138ff2f775f4c9e677d58cc3a65691aa83e5a
kubinoes <j.schovanec@protonmail.com>
2022-09-29 19:22:07 +0200

    Initial Commit

---
+++ File Encryptor/File_EncryptorApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_EncryptorApp.swift
+//  File Encryptor
+//
+//  Created by Jakub Schovanec on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct File_EncryptorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
