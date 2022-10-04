e649c0623453849d1f8d88082dddaea0df7bd396
kubinoes <j.schovanec@protonmail.com>
2022-10-04 17:40:15 +0200

    added File picker to select file to encrypt

--- File Encryptor/ContentView.swift
+++ File Encryptor/ContentView.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button("Encrypt a file") {
+                openFilePicker()
+            }
         }
         .padding()
     }
+    
+}
+
+func openFilePicker() {
+    let dialog = NSOpenPanel()
+    
+    dialog.title = "Choose a file you wish to encrypt"
+    dialog.showsResizeIndicator = true
+    dialog.showsHiddenFiles = false
+    dialog.allowsMultipleSelection = true
+    dialog.canChooseFiles = true
+    dialog.canChooseDirectories = true
+    
+    if dialog.runModal() == NSApplication.ModalResponse.OK {
+        let results = dialog.urls // pathname of the file
+        
+        for result in results {
+            print(result.path)
+        }
+    } else { return }
 }
 
 struct ContentView_Previews: PreviewProvider {
